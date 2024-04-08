22d5e725eb92542f1e1f345cdc23cb8401ac40d1
Min Goo <mg.choi.239@gmail.com>
2024-04-07 23:23:06 -0700

    data is showing but loss data fix required

--- FNN/training/training_abb05_nohebb_multilayer.py
+++ FNN/training/training_abb05_nohebb_multilayer.py
@@ -40,27 +40,24 @@ class SimpleNeuralNetwork(nn.Module):
         return torch.exp(-0.5 * (theta**2))
 
     def gaussian_rf(self, x):
-        theta_is = torch.linspace(0, 2 * torch.pi, self.input_size).view(-1,1)
+        theta_is = torch.linspace(0, 2 * torch.pi, self.input_sizes[0]).view(-1,1)
         return self.normal_pdf(x - theta_is) + self.normal_pdf(x - theta_is + 2 * torch.pi) + self.normal_pdf(x - theta_is - 2 * torch.pi)
 
     def forward(self, x):
         x1 = self.gaussian_rf(x)
-        print(self.gain)
-        print(self.shift)
+        self.l1 = self.activation_func(self.gain[0] * (x1 - self.shift[0]))
         
-        self.input_activation = self.activation_func(self.gain[0] * (x1 - self.shift[0]))
-        x2 = torch.matmul(self.weights[0], self.input_activation)
-
-        self.input_activation = self.activation_func(self.gain[1] * (x1 - self.shift[1]))
-        x2 = torch.matmul(self.weights[1], self.input_activation)
+        x2 = torch.matmul(self.weights[0], self.l1)
+        self.l2 = self.activation_func(self.gain[1] * (x2 - self.shift[1]))
         
-        self.output_activation = self.activation_func(3 * (x2 - 1))
-
+        x3 = torch.matmul(self.weights[1], self.l2)
+        self.output_activation = self.activation_func(3 * (x3 - 1))
+        
         return self.output_activation
 
     def train_epoch(self, xs, ys, hebbian_lr = 0.03, hebb_alpha = 5.5, oja_alpha = 1):
         # optimizer = optim.Adam([self.gain], lr=0.01)
-        optimizer = optim.SGD([self.gain, self.shift], lr=0.2)
+        optimizer = optim.SGD([self.gain[0], self.gain[1], self.shift[0], self.shift[1]], lr=0.2)
         loss_func = nn.MSELoss()
         epoch_loss = 0
         self.epoch += 1
@@ -82,11 +79,11 @@ class SimpleNeuralNetwork(nn.Module):
 
 ## RUN
 if __name__ == "__main__":
-    layers = [100, 100]
+    layers = [100, 10]
     init_gain = [3 * np.ones((nodes, 1)) for nodes in layers]
     init_shift = [1 * np.ones((nodes, 1)) for nodes in layers]
-    theo_gain = [3 * np.ones((nodes, 1)) for nodes in layers]
-    theo_shift = [1 * np.ones((nodes, 1)) for nodes in layers]
+    theo_gain = 3
+    theo_shift = 1
     init_weight = [np.ones((1, nodes)) * 5.5 / nodes for nodes in layers]
  
     # Data Generation, we will generate data points between 0 and 2*pi
@@ -97,10 +94,16 @@ if __name__ == "__main__":
     # Training Loop
     epochs = 100
     losses = []
-    gain_changes = []
-    shift_changes = []
-    gains = []
-    shifts = []
+    
+    gain_changes_0 = []
+    shift_changes_0 = []
+    gain_changes_1 = []
+    shift_changes_1 = []
+    
+    gains_0 = []
+    shifts_0 = []
+    gains_1 = []
+    shifts_1 = []
 
     for epoch in tqdm(range(epochs), position=0, leave=True):
         # establish model
@@ -115,20 +118,29 @@ if __name__ == "__main__":
         losses.append(epoch_loss)
 
         # update init
-        init_gain = model.gain.detach().numpy()
-        init_shift = model.shift.detach().numpy()
-        init_weight = model.weights.detach().numpy()
+        init_gain_0 = model.gain[0].detach().numpy()
+        init_shift_0 = model.shift[0].detach().numpy()
+        init_weight_0 = model.weights[0].detach().numpy()
+        
+        init_gain_1 = model.gain[1].detach().numpy()
+        init_shift_1 = model.shift[1].detach().numpy()
+        init_weight_1 = model.weights[1].detach().numpy()
 
         # print out info
         if epoch % 10 == 0:
             print(f'Epoch {epoch+1}/{epochs}, Loss: {epoch_loss}')
-            print(init_gain[0:10])
+            print(init_gain_0[0:10], init_gain_1[0:10])
         
         # record
-        gain_changes.append(np.linalg.norm(init_gain - theo_gain, 2))
-        shift_changes.append(np.linalg.norm(init_shift - theo_shift, 2))
-        gains.append(init_gain)
-        shifts.append(init_shift)
+        gain_changes_0.append(np.linalg.norm(init_gain_0 - theo_gain, 2))
+        shift_changes_0.append(np.linalg.norm(init_shift_0 - theo_shift, 2))
+        gains_0.append(init_gain_0)
+        shifts_0.append(init_shift_0)
+        
+        gain_changes_1.append(np.linalg.norm(init_gain_1 - theo_gain, 2))
+        shift_changes_1.append(np.linalg.norm(init_shift_1 - theo_shift, 2))
+        gains_1.append(init_gain_1)
+        shifts_1.append(init_shift_1)
 
     # true epoch
     epochs = epoch + 1
@@ -140,7 +152,13 @@ if __name__ == "__main__":
     with open(filepath, 'wb') as f:
         pickle.dump(model, f)
         pickle.dump(losses, f)
-        pickle.dump(gain_changes, f)
-        pickle.dump(shift_changes, f)
-        pickle.dump(gains, f)
-        pickle.dump(shifts, f)
+        
+        pickle.dump(gain_changes_0, f)
+        pickle.dump(shift_changes_0, f)
+        pickle.dump(gains_0, f)
+        pickle.dump(shifts_0, f)
+        
+        pickle.dump(gain_changes_1, f)
+        pickle.dump(shift_changes_1, f)
+        pickle.dump(gains_1, f)
+        pickle.dump(shifts_1, f)
